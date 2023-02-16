bd3d2941b1265f5a194a3c9155b9380d83808940
Likhitha733 <90965304+Likhitha733@users.noreply.github.com>
2023-02-16 22:53:04 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Likhitha\n";
+  cout << "I have successfully built and run\n";
+  cout << "And my name is Likhitha\n";
+  return 0;
+}
+
